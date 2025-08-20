2540b4d359e533b08efe378720705f92ead7b262
fukushi-mako <fukushi@nextreservation.jp>
2025-08-20 16:07:09 +0900

    テンプレート追加

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=1">
+    <title>テスト</title>
+</head>
+<body>
+<main>
+<div class="fv">
+    <hgroup>
+        <h1>○○のホームページ</h1>
+        <p>作成日：2025年8月20日（水）</p>
+    </hgroup>
+</div>
+<div class="content">
+    <h2>テキストテキストテキストテキスト</h2>
+    <div class="slide"></div>
+    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+</div>
+<div class="sidecon">
+    <ul class="side-list">
+        <li>
+            <img src="" alt="">
+            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </li>
+        <li>
+            <img src="" alt="">
+            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </li>
+    </ul>
+</div>
+<div class="link-box">
+    <iframe src="https://www.youtube.com/embed/AQuET3K4EGU?si=Wh44lTPHoOAug9mv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="youtube"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24132.505640650113!2d138.2080205500301!3d36.366498921041256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v1755668771885!5m2!1sja!2sjp" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+    <a href="https://nextreservation.jp/" class="link" target="link">リンクボタンだよ</a>
+</div>
+</main>
+<footer>
+    <p>©2025 NEXTRESERVATION</p>
+</footer>
+</body>
+</html>
